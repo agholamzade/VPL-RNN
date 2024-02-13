6eb554b903e289d08d2c3df8d412f66c91150b8d
ali gholamzadeh <aligholamzadeh42@gmail.com>
2024-02-13 15:34:35 -0500

    add pred loss

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ if __name__ == '__main__':
                 loss2 = loss_fn2(rnn_input, pred_out) 
                 # loss2 = loss_fn2(rnn_input, pred_out) + compute_var(pred_out)
 
-                if epoch < 40:
+                if epoch < 10:
                     train_loss = loss1 
                 else:
                     train_loss = loss1 + config["pred_mult"]*loss2
