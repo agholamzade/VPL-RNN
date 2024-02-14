eda5877a8b941088d5b8976cc8c0b22d70398403
ali gholamzadeh <aligholamzadeh42@gmail.com>
2024-02-14 01:41:28 -0500

    change to best settings

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
                 "batch_size": batch_size,
                 "lr": 2e-3,
                 "sep": train_sep,
-                "pred_mult": .02
+                "pred_mult": .01
         })
 
           # Copy your config
