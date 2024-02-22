01bef5e330fd2ee96369cd35c54a0bf4bdc3af78
ali gholamzadeh <aligholamzadeh42@gmail.com>
2024-02-22 01:43:06 -0500

    change it

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ if __name__ == '__main__':
                 "batch_size": batch_size,
                 "lr": 2e-3,
                 "sep": train_sep,
-                "pred_mult": .00005
+                "pred_mult": .001
         })
 
           # Copy your config
